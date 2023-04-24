a60439f66c94f58a24eef2c40e452129c2ece8bd
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-24 11:49:19 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateOutput() {
+  const output = document.getElementById('output');
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+
+  promise.then((message) => {
+    output.innerText = message;
+  });
+}
